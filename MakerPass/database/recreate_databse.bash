d8351112aa4a2e5e73485c130d286a01d87040a3
andersod2 <chrisemail127@gmail.com>
2015-05-04 22:36:26 -0700

    make recreate_database.bash script

--- MakerPass/database/recreate_databse.bash
+++ MakerPass/database/recreate_databse.bash
@@ -5,13 +5,16 @@
 ## download data from google docs
 wget --no-check-certificate https://docs.google.com/spreadsheets/d/1LyYnzCeJLjo9D6G46-hchiET48JVcDIRTxq5Ee-dhaE/export?format=csv -O ./machine_rec.csv
 
+## strip off header line of file
+sed -i '1d' machine_rec.csv
+
 ## create the tables in the database (database created automatically)
-cat makerpass_database.schema.sql | sqlite3 makerpass_datbase.db
+cat makerpass_database.schema.sql | sqlite3 makerpass_database.db
 
 ## import data into the database
-cat database_commands.import | sqlite3 makerpass_datbase.db
+cat database_commands.import | sqlite3 makerpass_database.db
 
 ## display results
-echo "select * from machine_rec;" | sqlite3 makerpass_datbase.db
+echo "select * from machine_rec;" | sqlite3 makerpass_database.db
 
 
